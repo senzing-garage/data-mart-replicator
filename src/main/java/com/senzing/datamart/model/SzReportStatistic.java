a5baad8bed046fa8d1e9cdac9f888013f7c95d96
Barry M. Caceres <barrycaceres@icloud.com>
2024-02-21 15:13:40 -0800

    Added SzReportStatistic.Formatter.parse() function to parse statistic

--- src/main/java/com/senzing/datamart/model/SzReportStatistic.java
+++ src/main/java/com/senzing/datamart/model/SzReportStatistic.java
@@ -107,6 +107,97 @@ public enum SzReportStatistic {
     }
 
     /**
+     * Gets the associated {@link SzReportStatistic}.
+     * 
+     * @return The associated {@link SzReportStatistic}.
+     */
+    public SzReportStatistic getStatistic() {
+      return this.statistic;
+    }
+
+    /**
+     * Gets the associated principle (if any).  If no associated principle
+     * then this returns <code>null</code>.
+     * 
+     * @return The associated principle, or <code>null</code> if none.
+     */
+    public String getPrinciple() {
+      return this.principle;
+    }
+
+    /**
+     * Gets the associated match key (if any).  If no associated match key
+     * then this returns <code>null</code>.
+     * 
+     * @return The associated match key, or <code>null</code> if none.
+     */
+    public String getMatchKey() {
+      return this.matchKey;
+    }
+
+    /**
+     * Parses an encoded statistic as an instance of {@link Formatter}.
+     * If the specified parameter is <code>null</code> or an empty string
+     * after trimming leading and trailing whitespace then <code>null</code>
+     * is returned.
+     * 
+     * @param encodedStatistic The encoded statistic.
+     *  
+     * @return The parsed statistic as a {@link Formatter}, or <code>null</code>
+     *         if the specified parameter is <code>null</code> or empty string.
+     * 
+     * @throws IllegalArgumentException If the specified encoded statistic is not
+     *                                  a properly formatted statistic.
+     */
+    public static Formatter parse(String encodedStatistic) {
+      if (encodedStatistic == null) return null;
+      String text = encodedStatistic.trim();
+      if (encodedStatistic.length() == 0) return null;
+
+      // get the statistic
+      SzReportStatistic statistic = null;
+      int index = text.indexOf(":");
+      if (index < 0) {
+        statistic = SzReportStatistic.valueOf(text);
+        text = "";
+
+      } else if (index == 0) {
+        throw new IllegalArgumentException(
+            "Improperly formatted report statistic: " + encodedStatistic);
+      } else {
+        String firstToken = text.substring(0, index);
+        firstToken = firstToken.trim();
+        statistic = SzReportStatistic.valueOf(firstToken);
+        text = (index == text.length() - 1) ? "" : text.substring(index+1);
+        text = text.trim();
+      }
+
+      // get the principle and match key
+      String principle = null;
+      String matchKey  = null;
+      
+      index = text.indexOf(":");
+      if (index < 0) {
+        principle = text;
+        matchKey  = null;
+
+      } if (index >= 0) {
+        principle = (index == 0) ? null : text.substring(0, index);
+        text = (index == text.length() - 1) ? "" : text.substring(index+1);
+        text = text.trim();
+        matchKey = (text.length() == 0) ? null : text;
+      }
+
+      // create the formatter
+      Formatter formatter = new Formatter(statistic);
+      if (principle != null) formatter.principle(principle);
+      if (matchKey != null) formatter.matchKey(matchKey);
+
+      // return the formatter
+      return formatter;
+    }
+
+    /**
      * Converts this instance to a {@link String} statistic.
      * 
      * @return The formatted {@link String} statistic for this instance.
